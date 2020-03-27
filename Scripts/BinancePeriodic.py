39bfcec74d2365e1fab94c944e7190bedff3d622
Julian Traversa <juliant@nescience.io>
2020-03-27 15:35:39 -0400

    v0.7.2
    -Bugfix Binance order size check

--- Scripts/BinancePeriodic.py
+++ Scripts/BinancePeriodic.py
@@ -413,8 +413,8 @@ def buy_order(asset, buy_asset, current_price):
 
     value = current_price * buy_asset
     if value >= 10:
-        if sell_asset == 'BTC_USDT':
-            sell_asset = truncate(sell_asset, 3)
+        if buy_asset == 'BTC_USDT':
+            buy_asset = truncate(buy_asset, 3)
         else:
             try:
                 currencies = client.get_products()['data']
